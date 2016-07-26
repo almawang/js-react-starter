a9f11cb3d935f0fd0ffa0b262ea20046c6bea8bf
Alma Wang <almawang96@gmail.com>
2016-07-25 23:41:40 -0400

    some authenication

--- src/firebasedb.js
+++ src/firebasedb.js
@@ -8,6 +8,20 @@ var config = {
 };
 firebase.initializeApp(config);
 
+// FirebaseUI config.
+var uiConfig = {
+  'signInSuccessUrl': 'https://localhost:8080/board',
+  'signInOptions': [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+};
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
 export function fetchNotes(callback) {
   firebase.database().ref('notes').on('value', (snapshot) => {
     callback(snapshot.val());
